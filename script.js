be089a2de66149fe249cf66b87eacb752e98d786
pedrohavieira <phav5002@gmail.com>
2025-04-22 19:59:18 -0300

    JS : Adição de evento de submit ao formulário para evitar o comportamento padrão

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ let gastos = [];
 const form = document.getElementById('form-gasto');
 const lista = document.getElementById('lista-gastos');
 const totalSpan = document.getElementById('total');
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    // ...
+  });
